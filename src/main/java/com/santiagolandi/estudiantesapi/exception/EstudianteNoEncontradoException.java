2631e90160fd52f0ae092a4b42d23f99a189d242
Santiago Landi <slandi1992@gmail.com>
2025-05-26 18:16:24 -0300

    Se completan metodos crud del service de estudiante y se crea la excepcion por estudiante no encontrado

--- src/main/java/com/santiagolandi/estudiantesapi/exception/EstudianteNoEncontradoException.java
+++ src/main/java/com/santiagolandi/estudiantesapi/exception/EstudianteNoEncontradoException.java
@@ -1,4 +1,7 @@
 package com.santiagolandi.estudiantesapi.exception;
 
-public class EstudianteNoEncontradoException {
+public class EstudianteNoEncontradoException extends RuntimeException {
+    public EstudianteNoEncontradoException(Long id) {
+        super("Estudiante no encontrado con id " + id);
+    }
 }
--- src/main/java/com/santiagolandi/estudiantesapi/service/EstudianteService.java
+++ src/main/java/com/santiagolandi/estudiantesapi/service/EstudianteService.java
@@ -3,11 +3,13 @@ package com.santiagolandi.estudiantesapi.service;
 
 import com.santiagolandi.estudiantesapi.dto.EstudianteDTO;
 import com.santiagolandi.estudiantesapi.entity.Estudiante;
+import com.santiagolandi.estudiantesapi.exception.EstudianteNoEncontradoException;
 import com.santiagolandi.estudiantesapi.mapper.EstudianteMapper;
 import com.santiagolandi.estudiantesapi.repository.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,7 +34,37 @@ public class EstudianteService {
     @Transactional(readOnly = true)
     public EstudianteDTO obtenerEstudiante(Long id){
         Estudiante estudiante = estudianteRepository.findById(id)
-                .orElseThrow(()-> new RuntimeException("Estudiante no encontrado con id: " + id) );
+                .orElseThrow(()-> new EstudianteNoEncontradoException(id));
         return estudianteMapper.toEstudianteDTO(estudiante);
+    }
+
+    @Transactional
+    public EstudianteDTO crearEstudiante(EstudianteDTO estudianteDTO){
+        Estudiante nuevoEstudiante = new Estudiante();
+        nuevoEstudiante.setNombre(estudianteDTO.getNombre());
+        nuevoEstudiante.setApellido(estudianteDTO.getApellido());
+        nuevoEstudiante.setDni(estudianteDTO.getDni());
+        nuevoEstudiante.setEmail(estudianteDTO.getEmail());
+        nuevoEstudiante.setCiudad(estudianteDTO.getCiudad());
+        estudianteRepository.save(nuevoEstudiante);
+        return estudianteMapper.toEstudianteDTO(nuevoEstudiante);
+    }
+
+    @Transactional
+    public EstudianteDTO actualizarEstudiante(Long id,EstudianteDTO estudianteDTO){
+        Estudiante buscado = estudianteRepository.findById(id).orElseThrow(()-> new EstudianteNoEncontradoException(id));
+        buscado.setNombre(estudianteDTO.getNombre());
+        buscado.setApellido(estudianteDTO.getApellido());
+        buscado.setDni(estudianteDTO.getDni());
+        buscado.setEmail(estudianteDTO.getEmail());
+        buscado.setCiudad(estudianteDTO.getCiudad());
+        estudianteRepository.save(buscado);
+        return estudianteMapper.toEstudianteDTO(buscado);
+    }
+
+    @Transactional
+    public void eliminarEstudiante(Long id){
+        Estudiante buscado = estudianteRepository.findById(id).orElseThrow(()-> new EstudianteNoEncontradoException(id));
+        estudianteRepository.delete(buscado);
     }
 }
