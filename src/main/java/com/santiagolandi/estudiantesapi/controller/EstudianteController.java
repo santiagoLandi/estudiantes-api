2ae359338ad70b05a602d578d0609b68a8e053b0
Santiago Landi <slandi1992@gmail.com>
2025-05-26 19:20:31 -0300

    Se completa el controlador de estudiante con el crud basico

---
+++ src/main/java/com/santiagolandi/estudiantesapi/controller/EstudianteController.java
@@ -0,0 +1,56 @@
+package com.santiagolandi.estudiantesapi.controller;
+
+import com.santiagolandi.estudiantesapi.dto.EstudianteDTO;
+import com.santiagolandi.estudiantesapi.service.EstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estudiante")
+public class EstudianteController {
+    private final EstudianteService estudianteService;
+
+    @Autowired
+    public EstudianteController(EstudianteService estudianteService) {
+        this.estudianteService = estudianteService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?>obtenerEstudiantes() {
+        List<EstudianteDTO>estudiantes = estudianteService.obtenerTodos();
+        if (estudiantes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(estudiantes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?>obtenerEstudiantePorId(@PathVariable Long id) {
+        EstudianteDTO buscado = estudianteService.obtenerEstudiante(id);
+        return ResponseEntity.ok(buscado);
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrarEstudiante(@RequestBody EstudianteDTO estudiante) {
+        estudianteService.crearEstudiante(estudiante);
+        return ResponseEntity.status(HttpStatus.CREATED).body(estudiante);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?>actualizarEstudiante(@RequestBody EstudianteDTO estudiante, @PathVariable Long id) {
+        estudianteService.actualizarEstudiante(id, estudiante);
+        return ResponseEntity.ok(estudiante);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarEstudiante(@PathVariable Long id) {
+        estudianteService.eliminarEstudiante(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
