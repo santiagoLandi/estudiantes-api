2b07d8a7eac291d6cd7de5bad395b4449d445722
Santiago Landi <slandi1992@gmail.com>
2025-05-27 14:44:28 -0300

    Eliminado Lombok por metodos explicitos. Proyecto funcionando OK

--- src/main/java/com/santiagolandi/estudiantesapi/dto/EstudianteDTO.java
+++ src/main/java/com/santiagolandi/estudiantesapi/dto/EstudianteDTO.java
@@ -2,25 +2,74 @@ package com.santiagolandi.estudiantesapi.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
+
 public class EstudianteDTO {
+
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+
     @NotBlank(message = "El apellido es obligatorio")
     private String apellido;
+
     @NotBlank(message = "El dni es obligatorio")
     private Long dni;
+
     @Email(message = "Debe ser un mail valido")
     @NotBlank(message = "El mail es obligatorio")
     private String email;
+
     @NotBlank(message = "La ciudad es obligatorio")
     private String ciudad;
+
+    public EstudianteDTO() {}
+
+    public EstudianteDTO(String nombre, String apellido, Long dni, String email, String ciudad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public Long getDni() {
+        return dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public @NotBlank(message = "La ciudad es obligatorio") String getCiudad() {
+        return ciudad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setDni(Long dni) {
+        this.dni = dni;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
 }
--- src/main/java/com/santiagolandi/estudiantesapi/entity/Estudiante.java
+++ src/main/java/com/santiagolandi/estudiantesapi/entity/Estudiante.java
@@ -1,13 +1,9 @@
 package com.santiagolandi.estudiantesapi.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Estudiante {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,4 +23,55 @@ public class Estudiante {
 
     @Column(nullable = false)
     private String ciudad;
+
+    public Estudiante() {}
+
+    public Estudiante(Long id, String nombre, String apellido, Long dni, String email, String ciudad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public Long getDni() {
+        return dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setDni(Long dni) {
+        this.dni = dni;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
 }
